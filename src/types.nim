54fa4ea8c1846f77d182b663a973feeff6a15115
Azumabashi <>
2023-08-08 07:33:28 +0900

    [feat] define type BeliefRevisionGame

--- src/types.nim
+++ src/types.nim
@@ -4,3 +4,8 @@ type
   Agent* = object
     id*: int
     belief*: PropLogicFormula
+  BeliefRevisionGame* = object
+    agents*: seq[Agent]
+    connection*: seq[seq[bool]]
+    atomicFormulae*: seq[PropLogicFormula]
+    revisionOperators*: seq[proc (self: PropLogicFormula, context: seq[PropLogicFormula]): PropLogicFormula]

bc05d88ec68c61dcbd39286c3939557e37c0de38
Azumabashi <>
2023-08-08 15:52:21 +0900

    add argument

--- src/types.nim
+++ src/types.nim
@@ -8,7 +8,7 @@ type
     agents*: seq[Agent]
     connection*: seq[seq[int]]
     atomicFormulae*: seq[PropLogicFormula]
-    revisionOperators*: seq[proc (self: PropLogicFormula, context: seq[PropLogicFormula], interpretations: seq[Interpretation]): PropLogicFormula{.nimcall, closure.}]
+    revisionOperators*: seq[proc (self: PropLogicFormula, context: seq[PropLogicFormula], interpretations: seq[Interpretation], allFormulae: seq[PropLogicFormula]): PropLogicFormula {.nimcall.}]
   RevisionOperatorConfig*[T] = object
     distance*: proc(x, y: Interpretation): float
     filter*: proc(x: seq[float]): T

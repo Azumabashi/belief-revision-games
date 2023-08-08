7696699f8a14643558f67d043b6c592a497d4a5e
Azumabashi <>
2023-08-08 12:33:26 +0900

    fix type definition

--- src/types.nim
+++ src/types.nim
@@ -8,7 +8,7 @@ type
     agents*: seq[Agent]
     connection*: seq[seq[bool]]
     atomicFormulae*: seq[PropLogicFormula]
-    revisionOperators*: seq[proc (self: PropLogicFormula, context: seq[PropLogicFormula], interpretations: seq[Interpretation]): PropLogicFormula]
+    revisionOperators*: seq[proc (self: PropLogicFormula, context: seq[PropLogicFormula], interpretations: seq[Interpretation]): PropLogicFormula{.nimcall.}]
   RevisionOperatorConfig*[T] = object
     distance*: proc(x, y: Interpretation): float
     filter*: proc(x: seq[float]): T

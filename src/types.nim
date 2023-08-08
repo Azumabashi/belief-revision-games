79d2246da5171f5f1072973b0ff03af71e842268
Azumabashi <>
2023-08-08 12:21:51 +0900

    add required parameter to type definition

--- src/types.nim
+++ src/types.nim
@@ -8,7 +8,7 @@ type
     agents*: seq[Agent]
     connection*: seq[seq[bool]]
     atomicFormulae*: seq[PropLogicFormula]
-    revisionOperators*: seq[proc (self: PropLogicFormula, context: seq[PropLogicFormula]): PropLogicFormula]
+    revisionOperators*: seq[proc (self: PropLogicFormula, context: seq[PropLogicFormula], interpretations: seq[Interpretation]): PropLogicFormula]
   RevisionOperatorConfig*[T] = object
     distance*: proc(x, y: PropLogicFormula): float
     filter*: proc(x: seq[float]): T

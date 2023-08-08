a49bd59272870cfdfaffa9098f3b34cdcd5b7306
Azumabashi <>
2023-08-08 15:29:42 +0900

    pass allFormulae to delta

--- src/revisionOperators.nim
+++ src/revisionOperators.nim
@@ -4,10 +4,15 @@ import math
 import types
 import distance
 
-proc revision3*(self: PropLogicFormula, context: seq[PropLogicFormula], interpretations: seq[Interpretation]): PropLogicFormula =
+proc revision3*(
+  self: PropLogicFormula,
+  context: seq[PropLogicFormula],
+  interpretations: seq[Interpretation],
+  allFormulae: seq[PropLogicFormula]
+): PropLogicFormula =
   let config = RevisionOperatorConfig[float](
     distance: drasticDistance,
     filter: proc(x: seq[float]): float = x.sum,
     cmp: cmp
   )
-  delta[float](config, self, context, interpretations)
+  delta[float](config, self, context, interpretations, allFormulae)

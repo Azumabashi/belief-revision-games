7587cb8530261768e057ded4c1b8eb146298d3ff
Azumabashi <>
2023-08-08 12:24:49 +0900

    change to correct type

--- src/types.nim
+++ src/types.nim
@@ -10,6 +10,6 @@ type
     atomicFormulae*: seq[PropLogicFormula]
     revisionOperators*: seq[proc (self: PropLogicFormula, context: seq[PropLogicFormula], interpretations: seq[Interpretation]): PropLogicFormula]
   RevisionOperatorConfig*[T] = object
-    distance*: proc(x, y: PropLogicFormula): float
+    distance*: proc(x, y: Interpretation): float
     filter*: proc(x: seq[float]): T
     cmp*: proc(x, y: T): int

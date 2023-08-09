fdc6a5d855ceacdcf394cfc45ae84727ce6a365b
Azumabashi <>
2023-08-09 16:16:21 +0900

    revise test program

--- tests/testRevision3.nim
+++ tests/testRevision3.nim
@@ -1,6 +1,9 @@
 import unittest
 import propositionalLogic
 import revisionOperators
+import types
+import distance
+import math
 
 suite "test for revision3":
   setup:
@@ -9,10 +12,15 @@ suite "test for revision3":
       s = allFormulae[0]
       b = allFormulae[1]
       q = allFormulae[2]
+      config = RevisionOperatorConfig[float](
+        distance: hammingDistance,
+        filter: proc(x: seq[float]): float = x.sum,
+        cmp: cmp
+      )
   
   test "revision by revision3":
     let 
       belief = s & (b => q)
       context = @[(!s) & b, !s]
-      newBelief = belief.revision3(context, allInterpretations, allFormulae)
+      newBelief = config.revision3(belief, context, allInterpretations, allFormulae)
     echo newBelief

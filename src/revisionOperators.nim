c6b8acb29e96bf558b20a370d425e38eeaef01d5
Azumabashi <>
2023-08-24 14:09:43 +0900

    [feat] define proc revision1

--- src/revisionOperators.nim
+++ src/revisionOperators.nim
@@ -3,6 +3,13 @@ import propositionalLogic
 import types
 import sequtils
 
+proc revision1*[T](
+  config: RevisionOperatorConfig[T],
+  self: PropLogicFormula,
+  context: seq[PropLogicFormula]
+): PropLogicFormula =
+  delta[T](config, self, context, interpretations().toSeq())
+
 proc revision3*[T](
   config: RevisionOperatorConfig[T],
   self: PropLogicFormula,

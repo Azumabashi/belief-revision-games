d41724a493a14b34b101641985d4fbb3430b76d1
Azumabashi <>
2023-08-24 17:58:35 +0900

    [feat] define proc gminConfig

--- src/belief_revision_games/configs.nim
+++ src/belief_revision_games/configs.nim
@@ -1,10 +1,22 @@
 import types
 import propositionalLogic
 import math
+import algorithm
 
 proc summentionConfig*(distance: proc(x, y: Interpretation): float): RevisionOperatorConfig[float] =
   RevisionOperatorConfig[float](
     distance: distance,
     filter: proc(x: seq[float]): float = x.sum,
     cmp: cmp
+  )
+
+proc gminConfig*(distance: proc(x, y: Interpretation): float): RevisionOperatorConfig[seq[float]] =
+  RevisionOperatorConfig[seq[float]](
+    distance: distance,
+    filter: proc(x: seq[float]): seq[float] = x.sorted(),
+    cmp: proc (x, y: seq[float]): int =
+      for i in 0..<x.len:
+        if x[i] > y[i]: return 1
+        elif x[i] == y[i]: continue
+        else: return -1
   )
